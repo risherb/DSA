bba39041f064f23819889d2d1a1fcb0eb7584357
Rishabh Shetty <133429575+risherb@users.noreply.github.com>
2024-10-22 18:34:53 +0530

    [2583] [Time Beats: 75.42%] [Memory Beats: 34.18%] - LeetPush

---
+++ DCP-10-24/2583-Kth-Largest-Sum-in-a-Binary-Tree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public long kthLargestLevelSum(TreeNode root, int k) {
+                Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        PriorityQueue<Long> minHeap = new PriorityQueue<>();
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            long levelSum = 0;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                levelSum += node.val;
+
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+
+            minHeap.add(levelSum);
+            if (minHeap.size() > k) minHeap.poll();
+        }
+
+        return minHeap.size() == k ? minHeap.peek() : -1;
+    }
+        
+    }
