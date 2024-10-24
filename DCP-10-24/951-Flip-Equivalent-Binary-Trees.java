967fe761851ae631f6b931ac3a8ec0c58966d73e
Rishabh Shetty <133429575+risherb@users.noreply.github.com>
2024-10-24 20:39:50 +0530

    [951] [Time Beats: 100.00%] [Memory Beats: 41.59%] - LeetPush

---
+++ DCP-10-24/951-Flip-Equivalent-Binary-Trees.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null || root1.val != root2.val) return false;
+        
+        boolean noFlip = flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
+        boolean flip = flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
+        
+        return noFlip || flip;
+    }
+}
