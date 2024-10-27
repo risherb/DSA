fb8ec76671063f6c003f4f25cb71a41d6ab31e1a
Rishabh Shetty <133429575+risherb@users.noreply.github.com>
2024-10-27 17:31:35 +0530

    [1277] [Time Beats: 45.12%] [Memory Beats: 85.04%] - LeetPush

---
+++ DCP-10-24/1277-Count-Square-Submatrices-with-All-Ones.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] dp = new int[rows][cols];
+        int count = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 1) {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1; // On the first row or first column, a '1' can only form a 1x1 square.
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                    }
+                    count += dp[i][j];
+                }
+            }
+        }
+        
+        return count;
+    }
+}
