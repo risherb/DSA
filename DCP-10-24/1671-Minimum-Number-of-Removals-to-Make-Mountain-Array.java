69b2b7d113fd8a263293d82b713a72ffe0d59fd3
Rishabh Shetty <133429575+risherb@users.noreply.github.com>
2024-10-30 12:43:53 +0530

    [1671] [Time Beats: 42.86%] [Memory Beats: 88.44%] - LeetPush

---
+++ DCP-10-24/1671-Minimum-Number-of-Removals-to-Make-Mountain-Array.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minimumMountainRemovals(int[] nums) {
+        int n = nums.length;
+        int[] lis = new int[n];
+        int[] lds = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            lis[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    lis[i] = Math.max(lis[i], lis[j] + 1);
+                }
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            lds[i] = 1;
+            for (int j = n - 1; j > i; j--) {
+                if (nums[i] > nums[j]) {
+                    lds[i] = Math.max(lds[i], lds[j] + 1);
+                }
+            }
+        }
+
+        int maxMountain = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (lis[i] > 1 && lds[i] > 1) {
+                maxMountain = Math.max(maxMountain, lis[i] + lds[i] - 1);
+            }
+        }
+
+        return n - maxMountain;
+    }
+}
